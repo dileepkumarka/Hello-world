08605870c9527edcc87daeb3e9747dfba57047af
dileep <dileepkumarka@gmail.com>
2019-11-08 00:36:45 +0530

    add common api file

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
-async function API(url, body, token){
-    const baseUrl = 'http://irateu.in:8000/api/';
-    const response = await fetch(baseUrl + url ,{
-        method: 'POST',
+async function API(url, params, token) {
+    const baseUrl = 'http://irateu.in:8080/api/';
+    const response = await fetch(`${baseUrl}${url}/${encodeURIComponent(params.client_id)}/${encodeURIComponent(params.branch_id)}`, {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Autherization': `Token ${token}`
+            'Authorization': `Token ${token}`
         },
-        body: JSON.stringify(body)
+        // body: JSON.stringify(body)
     })
     const data = await response.json();
     return data
--- src/typehead.js
+++ src/typehead.js
@@ -2,22 +2,65 @@ import React, { Component, Fragment } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import ReactDOM from 'react-dom';
 import 'react-bootstrap-typeahead/css/Typeahead.css';
+
+import API from './api/api';
 export default class Typehead extends Component {
     constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             selected: [],
+            dataSource: []
         }
     }
+
+    componentDidMount() {
+        this.fetchApi()
+    }
+
+    fetchApi() {
+        const dataSource = [];
+        const params = {
+            client_id: '5d84e063fe29594a592be4a8',
+            branch_id: '5d84e221fe29594a592be4ab'
+        }
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWQiOiI1ZDg0ZTM4MGZiYjJiZTc2ZWNlMzU4MWIiLCJleHAiOjE1NzM1ODAwMTMsImlhdCI6MTU3MzE0ODAxM30._lqdwEzsQjJFwnmeSBG-KAoorYP1uMnrPMmr9wPxBAs'
+        API('unhappylist', params, token)
+            .then((res) => {
+               res.reviewArr.map((e)=>{
+                dataSource.push(e.customer_name)
+                   this.setState({dataSource: dataSource})
+               })
+            })
+            .catch((err) => {
+                console.log("err", err);
+            })
+    }
+
+    onChangeValue = (val) => {
+        const params = {
+            client_id: '5d84e063fe29594a592be4a8',
+            branch_id: '5d84e221fe29594a592be4ab'
+        }
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZ21haWwuY29tIiwiaWQiOiI1ZDg0ZTM4MGZiYjJiZTc2ZWNlMzU4MWIiLCJleHAiOjE1NzM1ODAwMTMsImlhdCI6MTU3MzE0ODAxM30._lqdwEzsQjJFwnmeSBG-KAoorYP1uMnrPMmr9wPxBAs'
+        API('unhappylist', params, token)
+            .then((res) => {
+                console.log("res", res);
+            })
+            .catch((err) => {
+                console.log("err", err);
+            })
+    }
+
     render() {
+        const {dataSource} = this.state;
         return (
             <Typeahead
-        {...this.state}
-        id="basic-example"
-        onChange={selected => this.setState({ selected })}
-        options={this.props.options}
-        placeholder="Choose a state..."
-      />
+                {...this.state}
+                id="basic-example"
+                onChange={selected => this.onChangeValue(selected)}
+                options={dataSource}
+                placeholder="Select name"
+            />
         )
     }
 }
